910ad533f0aaadc4ede512170cf13f002a1f81d8
chenbw <opt_cbw@hotmail.com>
2022-05-05 12:47:31 +0800

    week03 example update ltc429

--- week03/example/ltc429_n_ary_tree_lever_order_traversal.py
+++ week03/example/ltc429_n_ary_tree_lever_order_traversal.py
@@ -24,3 +24,21 @@ class Solution:
             for i in node.children:
                 queue.append([i, depth + 1])
         return seq
+
+    def levelOrder2(self, root):
+        if root is None:
+            return []
+        from queue import Queue
+        q = Queue()
+        seq = []
+        q.put((root, 0))
+        while not q.empty():
+            node, depth = q.get()
+            if depth >= len(seq):
+                seq.append([])
+            seq[depth].append(node.val)
+            if not node.children:
+                continue
+            for child in node.children:
+                q.put((child, depth + 1))
+        return seq
